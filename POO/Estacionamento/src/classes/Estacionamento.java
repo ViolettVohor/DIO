5ebcb52be74c3529a7b7c503ee0c0fe4275a97d5
MatthewsTomts <learningahh@gmail.com>
2021-10-27 16:02:38 -0300

    first commit

---
+++ POO/Estacionamento/src/classes/Estacionamento.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Estacionamento {
+  double valorHora;
+  double totalFaturamento;
+  double totalFaturamentoCarro;
+  double totalFaturamentoMoto;
+  double totalFaturamentoUtilitario;
+}
---
+++ exemplo-gradle/src/main/java/br/com/mroho/exemplogradle/ExemploGradleApplication.java
@@ -0,0 +1,14 @@
+package br.com.mroho.exemplogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExemploGradleApplication.class, args);
+	}
+
+}
